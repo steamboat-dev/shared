a29fbff0137763e8e40b09e75b0b46d87e307ad9
Jamie Bishop <jamie@nullpixel.uk>
2017-04-18 14:59:29 +0100

    Add setting schema subdoc, move modlog to it

--- schema/guild.js
+++ schema/guild.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const InfractionSchema = require("./infractions");
+const SettingSchema = require('./settings');
 
 var GuildSchema = new Schema({
     guildID: {
         type: Number,
         required: true
     },
-    modLog: {
-        type: Number,
-        required: false
+    settings: {
+        type: [SettingSchema],
+        required: true
     },
     infractions: {
         type: [InfractionSchema],
---
+++ schema/settings.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var SettingSchema = new Schema({
+    modLogID: {
+        type: Number,
+        required: false
+    },
+    filterInvites: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+});
+
+module.exports = SettingSchema;
